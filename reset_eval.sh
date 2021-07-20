d58989aa551d6decf04c2e03f517f9b1d2517d42
K1S3L <kiselev_i@garpix.com>
2021-07-20 10:19:45 +0300

    reset licence jetbrains

---
+++ reset_eval.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# reset eval for all jetbrains product (Linux)
+# chmod 777 ./reset_jetbrains_eval.sh
+# sudo crontab -e -> insert at the end of the file: 0 10 1-7 * 1 * /path/to/reset_jetbrains_eval.sh
+
+OS_NAME=$(uname -s)
+if [[ $OS_NAME == "Linux" ]]; then
+        rm -rf ~/.config/JetBrains/${PRD}*
+        rm -rf ~/.java/.userPrefs/jetbrains/
+	rm -rf ~/.local/share/JetBrains
+	rm -rf ~/.cache/JetBrains
+        rm ~/.java/.userPrefs/prefs.xml
+else
+        echo 'Your OS is not Linux. Failed reset eval.'
+        exit
+fi
+
+echo 'Done.'
